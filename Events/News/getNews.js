8dc6979dae44deef8b9315b9d489f7660720953d
CryCry <cjl.hypoxia@gmail.com>
2023-02-25 00:28:16 +0800

    1120225_0

--- Events/News/getNews.js
+++ Events/News/getNews.js
@@ -11,7 +11,7 @@ module.exports = {
         const channel = guild.channels.cache.get(newschannelid);
         client.checkNews = async() => {
             const data = await parser.parseURL(`https://www.cdc.gov.tw/RSS/RssXml/Hh094B49-DRwe2RR4eFfrQ?type=1`).catch(console.error);
-            const rawdata = fs.readFileSync(`${__dirname}../../News/newsdata.json`);
+            const rawdata = fs.readFileSync(`${__dirname}/newsdata.json`);
             const jsondata = JSON.parse(rawdata);
             if (jsondata.link != data.items[0].link) {
                 fs.writeFileSync(`./Events/News/newsdata.json`,JSON.stringify({link: data.items[0].link}));
@@ -19,7 +19,7 @@ module.exports = {
                 const newstitle = data.items[0].title;
                 const embed = new EmbedBuilder().setTitle(newstitle).setURL(url).setColor('Random').setTimestamp();
                 await channel.send({embeds: [embed]})
-            } else return; //console.log('No News');
+            } else return;
         };
         setInterval(client.checkNews, 5 * 1000);
     }
--- Events/Youtube/checkVideo.js
+++ Events/Youtube/checkVideo.js
@@ -2,19 +2,21 @@ const {EmbedBuilder} = require('discord.js');
 const Parser = require('rss-parser');
 const fs = require('fs');
 const parser = new Parser();
-const {channelid_1} = require("../../video.json");
+const {guildid, newschannelid, youtubechannelid} = require('../../config.json');
 module.exports = {
     name: "ready",
     once: true,
     async execute(client) {
+        const guild = await client.guilds.cache.get(guildid);
+        const channel = guild.channels.cache.get(newschannelid);
         client.checkVideo = async() => {
-            const data = await parser.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelid_1}`).catch(console.error);
-            const rawdata = fs.readFileSync(`./video.json`);
+            const data = await parser.parseURL(`https://www.youtube.com/feeds/videos.xml?channel_id=${youtubechannelid}`).catch(console.error);
+            const rawdata = fs.readFileSync(`${__dirname}/video.json`);
             const jsondata = JSON.parse(rawdata);
             if (jsondata.id_1 != data.items[0].id) {
-                fs.writeFileSync(`./video.json`,JSON.stringify({id_1: data.items[0].id, channelid_1: channelid_1}))
-                //console.log('New video avaliable');
-            } else return;// console.log('no new video');
+                fs.writeFileSync(`./Events/Youtube/video.json`,JSON.stringify({id_1: data.items[0].id}))
+                await channel.send(`${data.items[0].link}`)
+            } else return;
         };
         setInterval(client.checkVideo, 6 * 1000);
     }
